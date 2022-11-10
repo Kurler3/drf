074b25f732fa09a3570d5f679ef35e21a4c45e9b
Kurler3 <miguelbento2000@gmail.com>
2022-11-10 08:59:50 +0000

    Creating product model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,22 @@
 from django.http import JsonResponse
+import json
 
 def api_home(req, *args, **kwargs):
     
+    # BYTE STRING OF JSON DATA
     body = req.body
     
-    print(body)
+    data = {}
     
-    return JsonResponse({"message": "Hi there, this is your Django API Response!!"})
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    
+    # HEADERS
+    data['params'] = dict(req.GET)
+    data['headers'] = dict(req.headers)
+    data['content_type'] = req.content_type
+    
+    return JsonResponse(data)
     
---
+++ backend/products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    # TITLE
+    title = models.CharField(max_length=120)
+    
+    # CONTENT (ALLOWS TO BE EMPTY OR NULL)
+    content = models.TextField(blank=True, null=True)
+        
+    # PRICE
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
