9032ba7d8b0c66578e4a13dc427463a4a2b6a61d
Kurler3 <miguelbento2000@gmail.com>
2022-11-12 10:59:56 +0000

    Working on model

--- backend/products/models.py
+++ backend/products/models.py
@@ -10,3 +10,7 @@ class Product(models.Model):
         
     # PRICE
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+    
+    @property
+    def sale_price(self):
+        return "%.2f" % (float(self.price) * 0.8)
